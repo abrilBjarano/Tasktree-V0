1509a71486dfea977e0aac0a9f9e9179323f7f54
abrilxcx <abrlbjrn@gmail.com>
2024-06-19 14:54:05 -0600

    Commented StrictMode in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import '../App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { TaskTree } from './TaskTree';
+
+import '../App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <TaskTree />
+  // </React.StrictMode>,
 )
