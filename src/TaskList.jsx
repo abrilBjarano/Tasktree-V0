efb3f5c21de22051d9553d7b36a4b88069612a4b
abrilxcx <abrlbjrn@gmail.com>
2024-06-21 12:21:59 -0600

    Added 'No tasks to show' in TaskList in case there's no tasks

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -4,17 +4,21 @@ export const TaskList = ({ tasks, onDeleteTask, onToggleTask }) => {
 
 
    return (
-      <ul className="list-group">
-         {
-            tasks.map( task => (
-               <TaskItem 
-                  key={ task.id } 
-                  task={ task } 
-                  onDeleteTask={ onDeleteTask }
-                  onToggleTask={ onToggleTask }
-               />
-            ))
-         }
-      </ul>
+      <>
+         <ul className="list-group">
+            {
+               tasks.map( task => (
+                  <TaskItem
+                     key={ task.id }
+                     task={ task }
+                     onDeleteTask={ onDeleteTask }
+                     onToggleTask={ onToggleTask }
+                  />
+               ))
+            }
+         </ul>
+
+         { tasks.length === 0 && <h1 className="text-center">No tasks to show</h1>}
+      </>
    )
 }
