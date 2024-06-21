2f6ce057e36176ccf16b84280532ff0d433003b2
abrilxcx <abrlbjrn@gmail.com>
2024-06-21 13:10:01 -0600

    Completed sheet cosmetic view

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -1,24 +1,30 @@
 export const TaskItem = ({ task, onDeleteTask, onToggleTask }) => {
 
-   
-   return (
-      <li 
-         className="list-group-item d-flex justify-content-between"
-         key={ task.id }
-      >
-         <span 
-            className={`align-self-center ${ ( task.done ) ?'text-decoration-line-through' : '' }`}
-            onClick={ () => onToggleTask( task.id ) }
-         >
-            { task.desc }
-         </span>
+   const itemStyle = {
+      backgroundColor: 'transparent', // Fondo transparente
+      border: '1px solid rgba(0, 0, 0, 0.1)', // Borde suave
+      width: '140%', // Ancho de cada elemento de la lista
+      margin: '0 auto', // Centrando cada elemento
+   };
 
-         <button 
+   return (
+      <li
+         className="list-group-item d-flex justify-content-between"
+         key={task.id}
+         style={itemStyle}
+      >
+         <span
+            className={`align-self-center ${ (task.done) ? 'text-decoration-line-through' : '' }`}
+            onClick={() => onToggleTask(task.id)}
+         >
+            {task.desc}
+         </span>
+         <button
             className="btn btn-danger"
-            onClick={ () => onDeleteTask( task.id )}
+            onClick={() => onDeleteTask(task.id)}
          >
             X
          </button>
       </li>
-   )
+   );
 }
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,24 +1,41 @@
 import { TaskItem } from "./TaskItem";
+import backgroundImage from './assets/papper.png';
 
 export const TaskList = ({ tasks, onDeleteTask, onToggleTask }) => {
 
+   const listStyle = {
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center calc(0% - 50px)',
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      paddingTop: '70px',
+      paddingRight: '82px',
+   };
+
+   const containerStyle = {
+      width: '67%', // Ancho del contenedor de la lista
+   };
 
    return (
-      <>
-         <ul className="list-group">
-            {
-               tasks.map( task => (
-                  <TaskItem
-                     key={ task.id }
-                     task={ task }
-                     onDeleteTask={ onDeleteTask }
-                     onToggleTask={ onToggleTask }
-                  />
-               ))
-            }
-         </ul>
-
-         { tasks.length === 0 && <h1 className="text-center">No tasks to show</h1>}
-      </>
-   )
+      <div style={listStyle}>
+         <div style={containerStyle}>
+            <ul className="list-group">
+               {
+                  tasks.map(task => (
+                     <TaskItem
+                        key={task.id}
+                        task={task}
+                        onDeleteTask={onDeleteTask}
+                        onToggleTask={onToggleTask}
+                     />
+                  ))
+               }
+            </ul>
+            {tasks.length === 0 && <h1 className="text-center">No tasks to show</h1>}
+         </div>
+      </div>
+   );
 }
